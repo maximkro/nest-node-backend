e8b83b35e688840d135fa7a7f48565ccd331e864
kikilove5me <77271656+kikilove5me@users.noreply.github.com>
2022-11-21 08:41:06 +0100

    auth module added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: `${process.env.PRIVATE_KEY}`,
+      signOptions: { expiresIn: '24h' }
+    })
+  ],
+  exports: [
+    AuthService,
+    JwtModule
+  ]
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcryptjs'
+import { User } from 'src/users/users.model';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private usersService: UsersService, private jwtService: JwtService) { }
+
+    async login(userDto: CreateUserDto) {
+    }
+
+
+    async registration(userDto: CreateUserDto) {
+        const candidate = await this.usersService.getUserByEmail(userDto.email);
+        if (candidate) {
+            throw new HttpException('email already exist', HttpStatus.BAD_REQUEST);
+        }
+
+        const hashPassword = await bcrypt.hash(userDto.password, 5);
+        const user = await this.usersService.createUser({ ...userDto, password: hashPassword });
+        return this.generateToken(user);
+
+    }
+
+    private async generateToken(user: User) {
+        const payload = { email: user.email, id: user.id, roles: user.roles };
+        return {
+            token: this.jwtService.sign(payload)
+        }
+    }
+}
